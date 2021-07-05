edc93c0d650a15bdfce3f51ed9cc6698ebb1d593
Volos Ivan <ivan.volos1@gmail.com>
2021-07-05 23:41:45 +0300

    Added firebase-tools

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import {Box, Container, Grid} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
 
 const Login = () => {
     return (
-        <div>
-            Login
-        </div>
+        <Container>
+            <Grid container
+                  style={{height: window.innerHeight - 50}}
+                  alignItems={"center"}
+                  justify={"center"}
+            >
+                <Grid container
+                    style={{width: 400, background: 'lightgrey'}}
+                    alignItems={"center"}
+                    direction={"column"}
+                    >
+                    <Box p={5}>
+                        <Button variant={"outlined"}>Log in with Google account</Button>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Container>
     );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import {Grid, Button} from "@material-ui/core";
+import {NavLink} from "react-router-dom";
+import {LOGIN_ROUTE} from "../utils/constants";
 
 const Navbar = () => {
+    const user = false;
     return (
-        <div>
-             Navbar
-        </div>
+        <AppBar color={"secondary"} position="static">
+            <Toolbar variant={"dense"}>
+                <Grid container justify={"flex-end"}>
+                    {user ?
+                        <Button variant={"outlined"}>Log out</Button>
+                        :
+                        <NavLink to={LOGIN_ROUTE}>
+                            <Button variant={"outlined"}>Log in</Button>
+                        </NavLink>
+                    }
+                </Grid>
+
+            </Toolbar>
+        </AppBar>
     );
 };
 
